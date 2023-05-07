3b65b5206f79f747ac9b9721f010a06578717261
LuisPulenta <luisalbertonu@gmail.com>
2023-05-07 12:38:43 -0300

    Video 281

---
+++ lib/presentation/screens/cubit_counter_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class CubitCounterScreen extends StatelessWidget {
+  const CubitCounterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('CubitCounterScreen'),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () => {},
+            icon: const Icon(Icons.refresh_outlined),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Counter value: xxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: 3,
+            onPressed: () => {},
+            child: const Text('+3'),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: 2,
+            onPressed: () => {},
+            child: const Text('+2'),
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          FloatingActionButton(
+            heroTag: 1,
+            onPressed: () => {},
+            child: const Text('+1'),
+          ),
+        ],
+      ),
+    );
+  }
+}
